ca430ccad056d8af9fce2154872822c1cdf7b39b
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2020-01-10 15:56:02 +0000

    try to install nltk data when running setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 import setuptools
+from setuptools.command.install import install as _install
+
+
+class Install(_install):
+    def run(self):
+        _install.do_egg_install(self)
+        import nltk
+        nltk.download("punkt")
 
 setuptools.setup(
     name="licalendarlib",
     version="0.1dev",
     packages=setuptools.find_packages(),
+    cmdclass = {"install" : Install},
     install_requires = [
         "nltk",
         "Pillow",
         "requests",
         "tqdm",
-        "pytz"]
+        "pytz"],
+    setup_requires = ["nltk"]
     )
